eb93f6841537ac5f8740337cf2b56f20a64a1732
meryemtnkt <36702284+meryemtnkt@users.noreply.github.com>
2021-05-28 16:29:34 +0300

    value
    referans ve deger

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class CustomerManager {
+
+	public void Add() {
+		System.out.println("Müþteri eklendi");
+	}
+	public void Remove() {
+		System.out.println("Müsteri silindi");
+	}
+	public void Update() {
+		System.out.println("Müþtri güncellendi");
+	}
+}
