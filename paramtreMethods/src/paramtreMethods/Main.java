9d9be90cc1ae27a022774e34254aee84f02e5e19
meryemtnkt <36702284+meryemtnkt@users.noreply.github.com>
2021-05-28 13:12:54 +0300

    temel java

--- paramtreMethods/src/paramtreMethods/Main.java
+++ paramtreMethods/src/paramtreMethods/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		System.out.println(yeniMesaj);
 		int sayi= topla(7,5);
 		System.out.println(sayi);
+		int toplam = topla2(2,3,4,5,6,10); //...sayilar[]
+		System.out.println(toplam);
 	}
 	public static void ekle() {
 		System.out.println("eklendi");
@@ -24,5 +26,12 @@ public class Main {
 	}
 	public static int topla(int sayi1 , int sayi2 ) {
 		return sayi1+sayi2;
+	}
+	public static int topla2(int... sayilar) { //variable arguments
+		int toplam=0;
+		for (int i : sayilar) {
+			toplam+=i;
+		}
+		return toplam;
 	}
 }
