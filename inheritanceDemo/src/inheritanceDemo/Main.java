b66680337873356f93977ff7874122f3edfd5167
meryemtnkt <36702284+meryemtnkt@users.noreply.github.com>
2021-06-01 02:11:54 +0300

    inheritance detail

---
+++ inheritanceDemo/src/inheritanceDemo/Main.java
@@ -0,0 +1,14 @@
+package inheritanceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		KrediUi krediUi= new KrediUi();
+		krediUi.KrediHesapla(new BaseKrediManager());
+		//polymorphism
+		krediUi.KrediHesapla(new AskerKrediManager());
+
+	}
+
+}
